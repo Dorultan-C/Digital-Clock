b1f216dce251d92e7911cb154fd7a04ee9cf1673
Cristian Dorultan <c.dorultan@gmail.com>
2022-07-25 21:36:05 +0100

    clock project finish

--- index.js
+++ index.js
@@ -13,4 +13,18 @@ function updateClock() {
     h = h-12
     ampm = "PM"
   }
+
+  h = h<10 ? "0" + h : h;
+  m = m<10 ? "0" + m : m;
+  s = s<10 ? "0" + s : s;
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondsEl.innerText = s;
+  ampmEl, (innerText = ampm);
+  setTimeout(()=>{
+updateClock()
+  }, 1000)
 }
+
+updateClock()
